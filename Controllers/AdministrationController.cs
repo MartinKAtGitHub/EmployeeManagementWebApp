bdecd13d9606beecc5342369826270df2ed35193
MartinK <martinsingh91@gmail.com>
2020-01-04 17:04:50 +0100

    Added Role Editing / updating

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Portfolio_Website_Core.Models;
 using Portfolio_Website_Core.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,13 @@ namespace Portfolio_Website_Core.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly ILogger<AdministrationController> logger;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager, ILogger<AdministrationController> logger)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, ILogger<AdministrationController> logger)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
             this.logger = logger;
         }
 
@@ -29,18 +32,18 @@ namespace Portfolio_Website_Core.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 IdentityRole identityRole = new IdentityRole
                 {
                     Name = model.RoleName
                 };
-                
+
                 IdentityResult result = await roleManager.CreateAsync(identityRole);
-                
-                if(result.Succeeded)
+
+                if (result.Succeeded)
                 {
-                    return RedirectToAction("ListRoles","Administration");
+                    return RedirectToAction("ListRoles", "Administration");
                 }
                 foreach (IdentityError error in result.Errors)
                 {
@@ -48,7 +51,7 @@ namespace Portfolio_Website_Core.Controllers
                 }
             }
 
-           
+
             return View();
         }
         [HttpGet]
@@ -57,5 +60,61 @@ namespace Portfolio_Website_Core.Controllers
             var roles = roleManager.Roles;
             return View(roles);
         }
+
+        // 80
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id{id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+            };
+
+            foreach (var user in userManager.Users.ToList<ApplicationUser>()) // await userManager.GetUsersInRoleAsync(role.Name)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name)) // gets an error data reader already in
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+        //80
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id{model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+               var result = await roleManager.UpdateAsync(role); // Remember to update the DB using this
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+
+        }
+
     }
 }
